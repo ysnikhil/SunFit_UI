1494ed40c97b0b171d1a64f0099bc18da1675cc7
nikhil_ys <nikhil.nyk007@gmail.com>
2019-01-02 19:16:10 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/sunfit_ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.sunfit_ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
